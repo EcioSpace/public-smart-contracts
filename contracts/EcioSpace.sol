85fa3ad1d5e251ba8e0d5dbd39ec923b569b095f
artizno <artiz@eico.space>
2021-12-13 18:35:04 +0700

    Remove upgradeable

--- contracts/EcioSpace.sol
+++ contracts/EcioSpace.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 interface IBPContract {
     function protect(
@@ -14,60 +14,22 @@ interface IBPContract {
     ) external;
 }
 
-contract EcioSpace is
-    Initializable,
-    ERC20Upgradeable,
-    ERC20BurnableUpgradeable,
-    PausableUpgradeable,
-    AccessControlUpgradeable
-{
+contract MyToken is ERC20, ERC20Burnable, Pausable, AccessControl {
+
     uint256 private constant TOTAL_SUPPLY = 7000 * 10**(6 + 18); // 7000M tokens
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public  constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public  constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+
+    //Bot prevention
     IBPContract public bpContract;
-
     bool public bpEnabled;
     bool public bpDisabledForever;
 
-    constructor() initializer {}
-
-    function initialize() public initializer {
-        __ERC20_init("ECIO Space", "ECIO");
-        __ERC20Burnable_init();
-        __Pausable_init();
-        __AccessControl_init();
-
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(PAUSER_ROLE, msg.sender);
-        _setupRole(MINTER_ROLE, msg.sender);
-    }
-
-    function setBPContract(address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(addr != address(0), "BP adress cannot be 0x0");
-        bpContract = IBPContract(addr);
-    }
-
-    function setBPEnabled(bool enabled) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        bpEnabled = enabled;
-    }
-
-    function setBPDisableForever() public onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(!bpDisabledForever, "Bot protection disabled");
-
-        bpDisabledForever = true;
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override whenNotPaused {
-        if (bpEnabled && !bpDisabledForever) {
-            bpContract.protect(from, to, amount);
-        }
-
-        super._beforeTokenTransfer(from, to, amount);
+    constructor() ERC20("ECIO Space", "ECIO") {
+        grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        grantRole(PAUSER_ROLE, msg.sender);
+        grantRole(MINTER_ROLE, msg.sender);
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -84,5 +46,33 @@ contract EcioSpace is
             "The token amount exceeded the total supply."
         );
         _mint(to, amount);
+    }
+
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override whenNotPaused {
+        if (bpEnabled && !bpDisabledForever) {
+            bpContract.protect(from, to, amount);
+        }
+
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function setBPContract(address addr) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(addr != address(0), "BP adress cannot be 0x0");
+        bpContract = IBPContract(addr);
+    }
+
+    function setBPEnabled(bool enabled) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        bpEnabled = enabled;
+    }
+
+    function setBPDisableForever() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(!bpDisabledForever, "Bot protection disabled");
+
+        bpDisabledForever = true;
     }
 }
